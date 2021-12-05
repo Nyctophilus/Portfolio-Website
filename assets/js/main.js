1aa17113147dee0ebd6b25c71c78ec303738c2ba
Medo <mohammed.yuossry@gmail.com>
2021-12-05 07:20:59 +0200

    Responsive mob View

--- assets/js/main.js
+++ assets/js/main.js
@@ -285,3 +285,58 @@ var player = document.getElementById('audio7');
   console.log("bird dead!");}
 
 }
+
+
+
+
+
+
+
+function floatHRT(){
+        var hrt = document.getElementById("hearts-alpaca");
+        hrt.classList.toggle("hearts-ff");
+}
+
+
+
+
+
+
+// Get the modal
+var modal = document.getElementById('myModal');
+
+// Get the image and insert it inside the modal - use its "alt" text as a caption
+var img = document.getElementById('myImg');
+var modalImg = document.getElementById("img01");
+var captionText = document.getElementById("caption");
+img.onclick = function(){
+    modal.style.display = "block";
+    modalImg.src = this.src;
+    modalImg.alt = this.alt;
+    captionText.innerHTML = this.alt;
+}
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+function getMeAcoffee() {
+        var GetMeDrink = document.getElementById("coffeeMac");
+        GetMeDrink.classList.toggle("drinkshow");
+}
